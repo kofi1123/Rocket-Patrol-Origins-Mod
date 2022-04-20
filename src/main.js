9135e0b90fc990186e36081429c9e3a6abbba9d3
Kofi Quansah <kquansah@ucsc.edu>
2022-04-20 02:47:34 -0700

    added coment to main

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
+/*
+    Name: Kofi Quansah
+    Project Title: Bubble Pop!
+    Date: 4/20/2021
+    TTF: 11 hrs
 
+    Point Breakdown:
+    Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+    Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+    Implement a new timing/scoring mechanism that adds time to the clock for successful hits (20)
+*/
 let config = {
     type: Phaser.CANVAS,
     width: 640,
